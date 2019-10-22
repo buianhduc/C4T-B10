be912a07c9b11526aad1cf2ae713ec858bdf0b47
Duc Bui <32723856+buianhduc@users.noreply.github.com>
2019-10-22 23:43:18 +0700

    Add files via upload

--- game/dungeonLV3.py
+++ game/dungeonLV3.py
@@ -5,27 +5,45 @@ Map = [
     ['-','-','-','-',],
     ['-','-','-','-',]
 ] #genarate map
-class condinate:
+class coordinate:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-Door = condinate(0,0)
-Key = condinate(0,0)
-Player = condinate(0,0)
-Wall = condinate(0,0)
-Key2 = condinate(0,0)
-Monster = condinate(0,0)
+Door = coordinate(0,0)
+Key = coordinate(0,0)
+Player = coordinate(0,0)
+Wall = coordinate(0,0)
+Key2 = coordinate(0,0)
+monster = coordinate(0,0)
+PlayerHP = 100
+MonsterHP = 100
 # => define cordinate of elements
-def same(condinates):
+
+def same(coordinates):
     result = True
-    for i in range(0,4):
-        for j in range(0,4):
-            if condinates[i][0] == condinates[i+1][0] and condinates[i][1] == condinates[i+1][1]:
+    for i in range(0,len(coordinates)-1):
+        for j in range(0,len(coordinates)-1):
+            if coordinates[i][0] == coordinates[i+1][0] and coordinates[i][1] == coordinates[i+1][1]:
                 result = False
     return result
-# => Check if there is a same condinate of diffrent elements
-def genarateCordinate(): #generate elements' condinate
+# => Check if there is a same coordinate of diffrent elements
+
+def monster_move():
+    command = random.randint(0,4)
+    order = 0
+    if command == 1:
+        order = 'w'
+    elif command == 2:
+        order = 's'
+    elif command == 3:
+        order = 'd'
+    elif command == 4:
+        order = 'a'
+    return order
+# => monster movement
+
+def genarateCordinate(): #generate elements' coordinate
     Door.x = random.randint(0,3)
     Door.y = random.randint(0,3)
     Key.x = random.randint(0,3)
@@ -35,33 +53,54 @@ def genarateCordinate(): #generate elements' condinate
     Wall.x = random.randint(0,3)
     Wall.y = random.randint(0,3)
     Key2.x = random.randint(0,3)
-    Key2.y = random.randint(0,3)
-    Monster.x = random.randint(0,3)
-    Monster.y = random.randint(0,3)
-    condinates = [[Door.x, Door.y], [Key.x,Key.y], [Player.x, Player.y], [Wall.x, Wall.y], [Key2.x,Key2.y], [monster.x, monster.y]]
-    if(same(condinates) == False):
+    Key2.y = random.randint(0,3)    
+    monster.x = random.randint(0,3)
+    monster.y = random.randint(0,3)
+    coordinates = [[Door.x, Door.y], [Key.x,Key.y], [Player.x, Player.y], [Wall.x, Wall.y], [Key2.x,Key2.y], [monster.x, monster.y]]
+    if(same(coordinates) == False):
         genarateCordinate()
-# => Generate condinate
-def move(entry): #update elements' condinate
-    if(entry == "w"):
-        if(Player.x > 0):
-            Player.x -= 1
-    elif(entry == 's'):
-        if(Player.x < 3):
-            Player.x += 1
-    elif(entry == 'd'):
-        if(Player.y < 3):
-            Player.y += 1
-    elif(entry == 'a'):
-        if(Player.y > 0):
-            Player.y -= 1
-# update condinate as the user type the move
-def updateOldCondinate(entry):
-    Map[Player.x][Player.y] = '-'
-    move(entry)
-    Map[Player.x][Player.y] = 'P'
-# update condinate on the map
+# => Generate coordinate
+def move(entry,kind): #update elements' coordinate
+    if kind == 'p':
+        if(entry == "w"):
+            if(Player.x > 0):
+                Player.x -= 1
+        elif(entry == 's'):
+            if(Player.x < 3):
+                Player.x += 1
+        elif(entry == 'd'):
+            if(Player.y < 3):
+                Player.y += 1
+        elif(entry == 'a'):
+            if(Player.y > 0):
+                Player.y -= 1 
+    if kind == 'm':
+        if(entry == "w"):
+            if(monster.x > 0):
+                monster.x -= 1
+        elif(entry == 's'):
+            if(monster.x < 3):
+                monster.x += 1
+        elif(entry == 'd'):
+            if(monster.y < 3):
+                monster.y += 1
+        elif(entry == 'a'):
+            if(monster.y > 0):
+                monster.y -= 1 
+# update coordinate as the user type the move
+def updateOldcoordinate(entry,kind):
+    if kind == 'p':
+        Map[Player.x][Player.y] = '-'
+        move(entry,'p')
+        Map[Player.x][Player.y] = 'P'
+    else:
+        Map[monster.x][monster.y] = '-'
+        move(entry,'m')
+        Map[monster.x][monster.y] = 'M'
+# update coordinate on the map
+    
 def printMap():
+    print("Your HP: {0}     {1} Monster's HP".format(PlayerHP, MonsterHP))
     for i in range(0, len(Map)):
         for j in range(0, len(Map)):
             print(Map[i][j], end =" ")
@@ -77,6 +116,16 @@ def convertEntry(entry):
     elif(entry == 's'):
         return 'w'
 # reverse user's order
+
+def deal_w_monster():
+    print('You have encontered with the monster!')
+    print('You have 3 options:')
+    print('1. Run')
+    print('2. Fight')
+    print('3. Defend')
+    movement = input('What do you wanna do?')
+
+
 if __name__ == "__main__":
     genarateCordinate()
     
@@ -88,12 +137,15 @@ if __name__ == "__main__":
     Map[Key.x][Key.y] = 'K'
     Map[Wall.x][Wall.y] = 'W'
     Map[Key2.x][Key2.y] = 'K2'
-
+    Map[monster.x][monster.y] = 'M'
     printMap()
 
     while(win == False):
         entry = input()
-        updateOldCondinate(entry)
+        updateOldcoordinate(entry,'p')
+        monster_move()
+        if (monster.x == Player.x and monster.y == Player.y):
+            deal_w_monster()
         if(Player.x == Key.x and Player.y == Key.y):
             HaveKey = True
         if(Player.x == Key2.x and Player.y == Key2.y):
@@ -101,13 +153,13 @@ if __name__ == "__main__":
 
         if(Player.x == Wall.x and Player.y == Wall.y):
             print("You have hit the frickin' wall")
-            updateOldCondinate(convertEntry(entry))
+            updateOldcoordinate(convertEntry(entry),'p')
             Map[Wall.x][Wall.y] = 'W'
-            
+        
         if(HaveKey == False or HaveKey2 == False):
             if(Player.x == Door.x and Player.y == Door.y):
                 print("You haven't pick up enough keys")
-                updateOldCondinate(convertEntry(entry))
+                updateOldcoordinate(convertEntry(entry),'p')
                 Map[Door.x][Door.y] = 'D'
         else:
             if(Player.x == Door.x and Player.y == Door.y):
